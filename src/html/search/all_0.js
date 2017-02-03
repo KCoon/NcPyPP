dcd646ad2581024021cf245ca53a60d6693e9b15
Sven Langer <mail@svenlanger.org>
2017-02-03 18:06:53 +0100

    remove docu out of src

--- src/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['foo',['foo',['../namespacencpypp.html#a9096b20ec561601b43f2e5d7d767d786',1,'ncpypp']]]
-];
--- src/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main',['main',['../namespacencpypp.html#adabbc623ee9dfc257e098933f580a2a4',1,'ncpypp']]]
-];
